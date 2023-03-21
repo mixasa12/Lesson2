df3f2d30883a4ec495a66e563f1e73242ebaedaf
mixasa12 <bandurinmm@gmail.com>
2023-03-21 21:30:24 +0300

    message

--- toastapp/src/main/java/ru/mirea/bandurin/toastapp/MainActivity.java
+++ toastapp/src/main/java/ru/mirea/bandurin/toastapp/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         String val = edit.getText().toString();
         int count = val.length();
         String countS=Integer.toString(count);
-        String result= "СТУДЕНТ No Х ГРУППА Х Количество символов - "+countS;
+        String result= "СТУДЕНТ No 4 ГРУППА БСБО-01-20 Количество символов - "+countS;
         Toast toast = Toast.makeText(getApplicationContext(),
                 result,
                 Toast.LENGTH_LONG);
